bb923007fdac724b35dacbcd12cb2e4e85704488
Nils Vogels <n.vogels@aves-it.nl>
2023-07-25 21:51:20 +0200

    Add vidattr to templates

---
+++ config/templates/surf/vidattr.php
@@ -0,0 +1,12 @@
+<?php
+
+global $vidattr;
+
+$vidattr = "";
+
+if (array_key_exists('vid', $_REQUEST)) {
+    $vid = $_REQUEST['vid'];
+    if( Utilities::isValidUID($vid) ) {
+        $vidattr = "&vid=" . $vid;
+    }
+}

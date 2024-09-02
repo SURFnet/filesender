00568efe9d2c5e65074b84f1ac4dfa86f48f6737
Nils Vogels <n.vogels@aves-it.nl>
2024-09-02 15:17:54 +0200

    Add essential changes from upstream

--- templates/exception_page.php
+++ templates/exception_page.php
@@ -1,10 +1,23 @@
 <?php
 
-if(!array_key_exists('exception', $_REQUEST))
+if(!array_key_exists('sid', $_REQUEST))
     throw new DetailedException('exception_not_found', '');
 
-$_REQUEST['exception'] = base64_encode(preg_replace('`\{(tr:)*(cfg|conf|config):([^}]+)\}`', '', base64_decode($_REQUEST['exception'])));
+$sid = $_REQUEST['sid'];
 
-$exception = StorableException::unserialize($_REQUEST['exception']);
+if (!preg_match('`^[0-9a-f]{13}$`', $sid)) {
+    throw new DetailedException('not_an_exception_identifier', $sid);
+}
+
+if (!array_key_exists('exception_'.$sid, $_SESSION)) {
+    throw new DetailedException('unknown_exception_identifier', $sid);
+}
+
+$exception = $_SESSION['exception_'.$sid];
+if (!($exception instanceof Exception)) {
+    throw new DetailedException('not_an_exception', $sid);
+}
+
+$exception = $_SESSION['exception_'.$sid];
 
 Template::display('exception', array('exception' => $exception));
